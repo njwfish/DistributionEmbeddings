354f726f40de34d1b222bdb9d52acd7a645251d0
njwfish <njwfish@node1702.inband>
2025-04-19 00:48:27 -0400

    added context masking to trainer

--- training.py
+++ training.py
@@ -20,6 +20,7 @@ class Trainer:
         early_stopping=True,
         patience=10,
         use_tqdm=True,
+        mask_context_prob=0.0
     ):
         """
         Initialize the trainer.
@@ -40,6 +41,7 @@ class Trainer:
         self.early_stopping = early_stopping
         self.patience = patience
         self.use_tqdm = use_tqdm
+        self.mask_context_prob = mask_context_prob
         
         self.logger = logging.getLogger(__name__)
         self.best_loss = float('inf')
@@ -132,6 +134,9 @@ class Trainer:
                 if isinstance(batch['samples'], torch.Tensor):
                     samples = batch['samples'].to(device)
                     latent = encoder(samples)  # latent is num samples x num sets x latent dim
+                    if self.mask_context_prob > 0:
+                        context_mask = torch.bernoulli(torch.zeros(latent.shape[0])+self.mask_context_prob).to(latent.device)
+                        latent = latent * context_mask[:, None]
                     loss = generator.loss(samples.view(-1, *samples.shape[2:]), latent)
                 else:
                     # For dictionary samples (like PubMed dataset), move tensors to device
@@ -144,7 +149,9 @@ class Trainer:
 
                     # Encode samples to latent space
                     latent = encoder(samples)
-                    
+                    if self.mask_context_prob > 0:
+                        context_mask = torch.bernoulli(torch.zeros(latent.shape[0])+self.mask_context_prob).to(latent.device)
+                        latent = latent * context_mask
                     # Calculate loss
                     loss = generator.loss(samples, latent)
                 
