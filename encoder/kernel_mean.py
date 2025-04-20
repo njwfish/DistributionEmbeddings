70bd5b2d263c2aa10e657e92604b5b915f9e8a22
gokulg <11763361+ggdna@users.noreply.github.com>
2025-04-20 17:32:06 -0400

    kme

---
+++ encoder/kernel_mean.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+class KMEEncoder(nn.Module):
+    def __init__(self, gamma=0.1, d=32, seed=None, device='cuda'):
+        super().__init__()
+        self.gamma = gamma
+        self.d = d//2
+        self.seed = seed  # optional! for reproducibility
+
+    def forward(self, x):
+        # x: (batch, set_size, N_dims)
+
+        x = x.flatten(start_dim=2) # for mnist for now
+
+        batch, set_size, N_dims = x.shape
+
+        # sample random weights for RFF (new for each forward pass!)
+        if self.seed is not None:
+            torch.manual_seed(self.seed)
+        scale = 1.0 / self.gamma
+        W = torch.randn(N_dims, self.d, device=x.device) * scale  # (N_dims, d)
+
+        # project + apply RFFs
+        XW = torch.matmul(x, W)  # (batch, set_size, d)
+        cos = torch.cos(XW)
+        sin = torch.sin(XW)
+        phi = torch.cat([cos, sin], dim=-1)  # (batch, set_size, 2d)
+
+        # mean across the set
+        return phi.mean(dim=1)  # (batch, 2d)
