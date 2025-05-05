87a04e647383d81d3910cbfab620e1df26b54b10
njwfish <njwfish@node1703.inband>
2025-05-05 10:57:37 -0400

    updates

--- encoder/mean.py
+++ encoder/mean.py
@@ -6,12 +6,13 @@ class MeanEncoder(nn.Module):
     def __init__(self, data_shape, d=32, seed=None):
         super().__init__()
         self.seed = seed
+        self.d = d
         self.N_dims = np.prod(data_shape)
         self.proj = torch.nn.Parameter(torch.randn(self.N_dims, self.d))
 
     def forward(self, x):
         # x: (batch, set_size, N_dims)
         x = x.flatten(start_dim=2) # for mnist for now
-        x = x.mean(dim=2)
+        x = x.mean(dim=1)
         x = torch.matmul(x, self.proj)
         return x
