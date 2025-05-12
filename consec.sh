bff33a9c6f4f2836da3090891230c084d845c568
gokulg <11763361+ggdna@users.noreply.github.com>
2025-05-12 14:44:04 -0400

    image benchmarking

--- consec.sh
+++ consec.sh
@@ -1,11 +1,14 @@
-for enc in wormhole_encoder conv_gnn kernel_mean mean; do
-  python main.py experiment=fmnist_sys_diff encoder=$enc
+for enc in conv_gnn kernel_mean; do
+  python main.py experiment=fmnist_sys_diff encoder=$enc training.max_time=900
 done
+python main.py experiment=fmnist_sys_wormhole encoder=wormhole_encoder training.max_time=900
 
-for enc in wormhole_encoder conv_gnn kernel_mean mean; do
-  python main.py experiment=fmnist_sys_wormhole encoder=$enc
+for enc in conv_gnn kernel_mean; do
+  python main.py experiment=mnist_sys_diff encoder=$enc training.max_time=900
 done
+python main.py experiment=mnist_sys_wormhole encoder=wormhole_encoder training.max_time=900
 
-for enc in wormhole_encoder conv_gnn kernel_mean mean; do
-  python main.py experiment=fmnist_sys_vae encoder=$enc
+for enc in resnet kernel_mean; do
+  python main.py experiment=gmm_sys_diff encoder=$enc training.max_time=600
 done
+python main.py experiment=gmm_sys_wormhole encoder=wormhole_encoder training.max_time=600
