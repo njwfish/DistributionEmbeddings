7e89b483d74e9e14f1a6da656e90b9d271874737
gokulg <11763361+ggdna@users.noreply.github.com>
2025-05-13 10:12:23 -0400

    cifar

--- consec.sh
+++ consec.sh
@@ -1,14 +1,4 @@
 for enc in conv_gnn kernel_mean; do
-  python main.py experiment=fmnist_sys_diff encoder=$enc training.max_time=900
+  python main.py experiment=cifar_sys_diff encoder=$enc training.max_time=1200
 done
-python main.py experiment=fmnist_sys_wormhole encoder=wormhole_encoder training.max_time=900
-
-for enc in conv_gnn kernel_mean; do
-  python main.py experiment=mnist_sys_diff encoder=$enc training.max_time=900
-done
-python main.py experiment=mnist_sys_wormhole encoder=wormhole_encoder training.max_time=900
-
-for enc in resnet kernel_mean; do
-  python main.py experiment=gmm_sys_diff encoder=$enc training.max_time=600
-done
-python main.py experiment=gmm_sys_wormhole encoder=wormhole_encoder training.max_time=600
+python main.py experiment=cifar_sys_wormhole encoder=wormhole_encoder training.max_time=1200
