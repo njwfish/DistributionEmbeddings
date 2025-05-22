b0775bf2b39b1ce8f2c669cb732ff0e270d53810
gokulg <11763361+ggdna@users.noreply.github.com>
2025-05-22 11:59:45 -0400

    wishart scale

--- consec.sh
+++ consec.sh
@@ -1,4 +1,13 @@
-for enc in conv_gnn kernel_mean; do
-  python main.py experiment=cifar_sys_diff encoder=$enc training.max_time=1200
+for i in {5..5}; do
+
+    # python main.py --multirun experiment=mvn_sys_diff encoder=mean,kernel_mean,gnn,median_gnn,resnet,tx experiment.name="mvn_sys_diff_$i" training.max_time=300 dataset.prior_cov_scale=0.1
+
+    python main.py --multirun experiment=mvn_sys_sinkhorn encoder=mean,kernel_mean,gnn,median_gnn,resnet,tx experiment.name="mvn_sys_sinkhorn_$i" training.max_time=300 dataset.prior_cov_scale=0.1
+
+    python main.py --multirun experiment=mvn_sys_sw encoder=mean,kernel_mean,gnn,median_gnn,resnet,tx experiment.name="mvn_sys_sw_$i" training.max_time=300 dataset.prior_cov_scale=0.1
+
+    python main.py --multirun experiment=mvn_sys_vae encoder=mean,kernel_mean,gnn,median_gnn,resnet,tx experiment.name="mvn_sys_vae_$i" training.max_time=300 dataset.prior_cov_scale=0.1
+
+    python main.py --multirun experiment=mvn_sys_wormhole encoder=mean,kernel_mean,gnn,median_gnn,resnet,tx experiment.name="mvn_sys_wormhole_$i" training.max_time=300 dataset.prior_cov_scale=0.1
+
 done
-python main.py experiment=cifar_sys_wormhole encoder=wormhole_encoder training.max_time=1200
